0605910f1373328771cafed98d11535a77a40f7f
Thiago Loiola <bens.quin@gmail.com>
2026-09-24 15:03:03 -0300

    agagag

--- src/components/backgroundParallax1/index.tsx
+++ src/components/backgroundParallax1/index.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./Styles.module.css";
 
 export function BackgroundParallax1() {
+  const [offsetY, setOffsetY] = useState<number>(0);
+
+  //  Adicione <HTMLDivElement> aqui
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!ref.current) return;
+
+      window.requestAnimationFrame(() => {
+        if (!ref.current) return;
+        // Agora o TypeScript sabe que ref.current é um elemento DIV e aceita o método
+        const rect = ref.current.getBoundingClientRect();
+        setOffsetY(rect.top * 0.15);
+      });
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <>
-      <div className={styles.backgroudParallax}>
-        <span></span>
-      </div>
-    </>
+    <div ref={ref} className={styles["parallax-wrapper"]}>
+      <div
+        className={styles["parallax-bg"]}
+        style={{ transform: `translate3d(0, ${offsetY}px, 0)` }}
+      />
+      <div className={styles["parallax-content"]}></div>
+    </div>
   );
 }
