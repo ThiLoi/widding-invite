52dba02a2f4405518ae4b95fe642e968e6a8d367
Thiago Loiola <bens.quin@gmail.com>
2026-09-24 23:52:02 -0300

    24/09/2026 transformei a logo em html

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,16 @@
-import TextoPrincipal from "./Texto Principal.jpg";
+import Logo from "./imgs/Logo.svg";
 import styles from "./Styles.module.css";
 
 export function Heading() {
   return (
     <div className={styles.heading}>
-      <img src={TextoPrincipal} alt="" />
+      <div className={styles.bordas}>
+        <img src={Logo} alt="Logo" />
+        <h1>
+          MATHEUS <br /> & <br /> WÂNIA
+        </h1>
+        <h2>12 | 12 |2026</h2>
+      </div>
     </div>
   );
 }
