cec22412783bb79b2bb999b6e35f2c9cb4129cec
Thiago Loiola <bens.quin@gmail.com>
2026-09-26 00:39:37 -0300

    26/09/2025 Mudança de logo

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -6,6 +6,10 @@ export function Heading() {
     <div className={styles.heading}>
       <div className={styles.bordas}>
         <img src={Logo} alt="Logo" />
+        <h1>
+          MATHEUS <br /> & <br /> WÂNIA
+        </h1>
+        <h2>12 | 12 |2026</h2>
       </div>
     </div>
   );
